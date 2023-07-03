aff40458ae5ed731efdc8e71aa597da933e9c23c
Vaibhav Yergude <128138409+vaibhavyergude@users.noreply.github.com>
2023-07-03 18:26:20 +0530

    Add files via upload

---
+++ Frontend/src/components/PropertyUpdate/PropertyUpdate.js
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import NavbarOther from "../NavbarOther/NavbarOther";
+
+const PropertyForm = () => {
+	const [num, setNum] = useState("");
+	const [price, setPrice] = useState("");
+
+	const handleSubmit = async (event) => {
+		event.preventDefault();
+
+		try {
+			const response = await fetch("/property-update", {
+				method: "PUT",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({ num, price }),
+			});
+
+			if (response.ok) {
+				const updatedProperty = await response.json();
+				console.log(updatedProperty); // Assuming the response contains the updated document
+			} else {
+				throw new Error("Failed to update property");
+			}
+		} catch (error) {
+			console.error(error);
+		}
+	};
+
+	return (
+		<div>
+			<NavbarOther />
+			<div className="row justify-content-center">
+				<form onSubmit={handleSubmit} className="mt-5 col-6 text-center">
+					<div className="mb-3">
+						<label htmlFor="num" className="form-label">
+							Number:
+						</label>
+						<input
+							type="text"
+							id="num"
+							className="form-control"
+							value={num}
+							onChange={(event) => setNum(event.target.value)}
+						/>
+					</div>
+					<div className="mb-3">
+						<label htmlFor="price" className="form-label">
+							Price:
+						</label>
+						<input
+							type="text"
+							id="price"
+							className="form-control"
+							value={price}
+							onChange={(event) => setPrice(event.target.value)}
+						/>
+					</div>
+					<button type="submit" className="btn btn-primary">
+						Update
+					</button>
+				</form>
+			</div>
+		</div>
+	);
+};
+
+export default PropertyForm;
